53fae64fac039aa63b35249ecc7fc21d42d7b4ff
Faydiamond <faba_1512@hotmail.com>
2016-05-11 13:07:13 -0500

    Reconocimiento de voz beta

---
+++ ReconoceVoz/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Recognition;
+
+namespace ReconoceVoz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private SpeechRecognitionEngine recorder = new SpeechRecognitionEngine();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grabar_Click(object sender, RoutedEventArgs e)
+        {
+            //boton garbar:
+
+            //Declaro el dispositivo
+            recorder.SetInputToDefaultAudioDevice();
+            //carga la gramatica
+            recorder.LoadGrammar(new DictationGrammar());
+            //
+            recorder.SpeechRecognized += recorder_SpeechRecognized; //revisar aca
+            recorder.RecognizeAsync(RecognizeMode.Single);
+
+
+
+
+        }
+
+        void recorder_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            foreach (RecognizedWordUnit word in e.Result.Words)
+            {
+                Resultados.Items.Add(word.Text);
+
+                if (word.Text == "siguiente")
+                {
+
+                    Prueba p = new Prueba();
+                    p.Show();
+                    this.Close();
+                    //MessageBox.Show("Esatas adentro, sos bienvenido");
+                    //corder.RecognizeAsyncCancel();
+                    //corder.SetInputToNull();
+
+                }
+                else
+                {
+                    MessageBox.Show("Usted no puede ingresar a la aplicacion");
+                }
+
+            }
+        }
+    }
+}
---
+++ ReconoceVoz/Prueba.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+
+namespace ReconoceVoz
+{
+    /// <summary>
+    /// Interaction logic for Prueba.xaml
+    /// </summary>
+    public partial class Prueba : Window
+    {
+          KinectSensorChooser miKinect;  //verifico  cuantos kinect  estan  conectados
+        public Prueba()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            miKinect = new KinectSensorChooser();                   //creo objeto
+            miKinect.KinectChanged += miKinect_KinectChanged;       //si se deconecto un kinect o s einicializo
+            sensorChooserUI.KinectSensorChooser = miKinect; //verifica si esta conectado o desconectado en pantalla
+            miKinect.Start();
+        }
+
+        void miKinect_KinectChanged(object sender, KinectChangedEventArgs e)
+        {
+
+            //se desconecto el kinect
+            bool error = true;                                                          //verifico error
+            if (e.OldSensor == null)
+            {                                                      // se desconecto
+                try
+                {
+                    e.OldSensor.DepthStream.Disable();                                      //desabilito los streams  que habia habilitado
+                    e.OldSensor.SkeletonStream.Disable();
+                }
+                catch (Exception)
+                {
+
+                    error = true;                                                           //error  es igual a true
+                }
+            }
+
+            if (e.NewSensor == null)
+                return;
+            try
+            {
+                e.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);            //asigno el formato del strin de profundidad 640*480
+                e.NewSensor.SkeletonStream.Enable();
+                try
+                {
+                    e.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;          //se habilita
+                    e.NewSensor.DepthStream.Range = DepthRange.Near;                                  //  habilito el nearmode solo en string de profundidad mas rango de datos
+                    e.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;                      // se habilita y se hace verdadera  (solo para kinect de windows)
+                }
+                catch (InvalidOperationException)
+                {
+
+                    e.NewSensor.DepthStream.Range = DepthRange.Default;                                  //desactivo esos modos de range   
+                    e.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                error = true;
+
+            }
+
+            ZonaCursor.KinectSensor = e.NewSensor;
+        }
+
+        private void KinectTileButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Muay Thai is the melody of my life");
+        }
+
+        private void KinectTileButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(); //salir
+        }
+
+
+    }
+}
